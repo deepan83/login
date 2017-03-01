fab219c3d28f69b9e8863efa3e53eb6be7e5ceed
deepan83 <deepan.cts@gmail.com>
2017-03-01 19:11:07 +0000

    jsessionid and token

--- index.ios.js
+++ index.ios.js
@@ -7,12 +7,16 @@ import {
   Button,
   ActivityIndicator
 } from 'react-native';
+var CookieManager = require('react-native-cookies');
+
 
 export default class login extends Component {
   constructor() {
     super();
     this.state = {
       customer: null,
+      jsessionid: null,
+      access_token: null,
       animating: false
     }
     this.fetchAPI = this.fetchAPI.bind(this);
@@ -38,23 +42,54 @@ export default class login extends Component {
         if (!response.ok) {
           throw new Error(err);
         }
-        response.json().then((response) => {
-          let headers = new Headers();
-          headers.set('Authorization', `Bearer ${response.access_token}`);
-          headers.set('X-Client-ID', 'cbdcfa43-dd67-4c38-b418-83572a936fca');
-          headers.set('Accept', 'application/vnd.api+json, application/json');
+        return response.json();
+      })
+      .then((response) => {
+        var headers = new Headers();
 
-          fetch('https://treatsmaproxy.herokuapp.com/v1/customers', {
-            headers
-          })
-          .then((response) => {
-            return response.json();
-          })
-          .then((response) => {
-            this.setState({'customer': response.data[0]});
-            this.setState({animating: false});
-          })
+        CookieManager.get('https://treatsmaproxy.herokuapp.com/', (err, res) => {
+          this.setState({jsessionid: res.JSESSIONID});
         });
+
+        headers.set('Authorization', 'Basic Y2JkY2ZhNDMtZGQ2Ny00YzM4LWI0MTgtODM1NzJhOTM2ZmNhOjdlZDUwOTcxLWNiOGQtNGViZi1iZWUwLTQ5MTM1NDRjZjg5NA==');
+        headers.set('Content-Type', 'application/x-www-form-urlencoded');
+
+        var body = 'username=fitbit@bgdigitaltest.co.uk&password=password12&grant_type=password';
+
+        return fetch('https://treatsmaproxy.herokuapp.com/token', {
+          method: 'POST',
+          headers: headers,
+          body: body
+        })
+      })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(err);
+        }
+        return response.json();
+      })
+      .then((response) => {
+        let headers = new Headers();
+
+        this.setState({access_token: response.access_token});
+
+        headers.set('Authorization', `Bearer ${response.access_token}`);
+        headers.set('X-Client-ID', 'cbdcfa43-dd67-4c38-b418-83572a936fca');
+        headers.set('Accept', 'application/vnd.api+json, application/json');
+
+        return fetch('https://treatsmaproxy.herokuapp.com/v1/customers', {
+          headers
+        });
+      })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(err);
+        }
+        return response.json();
+      })
+      .then((response) => {
+        this.setState({'customer': response.data[0]});
+        this.setState({animating: false});
       })
       .catch((err) => {
         console.log('error');
@@ -85,6 +120,12 @@ export default class login extends Component {
           size="large"
          />
         {this.renderCustomerDetails()}
+        <Text style={styles.instructions}>
+         JSESSIONID: {this.state.jsessionid}
+        </Text>
+        <Text style={styles.instructions}>
+         access_token: {this.state.access_token}
+        </Text>
       </View>
     );
   }
